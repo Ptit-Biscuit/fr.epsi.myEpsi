dd8e8d2d351f5c0a3497b72eefbce52f633ba68c
Ptit-Biscuit <vincent.brebion@etu.univ-nantes.fr>
2018-03-23 12:33:23 +0100

    Added tests

---
+++ test/fr/epsi/myEpsi/controlers/database/implementations/DaoImplTest.java
@@ -0,0 +1,14 @@
+package fr.epsi.myEpsi.controlers.database.implementations;
+
+import org.junit.jupiter.api.Test;
+
+class DaoImplTest {
+
+	@Test
+	void connectionAlive() {
+	}
+
+	@Test
+	void closeConnection() {
+	}
+}
---
+++ test/fr/epsi/myEpsi/controlers/database/implementations/UserDaoImplTest.java
@@ -0,0 +1,26 @@
+package fr.epsi.myEpsi.controlers.database.implementations;
+
+import org.junit.jupiter.api.Test;
+
+class UserDaoImplTest {
+
+	@Test
+	void create() {
+	}
+
+	@Test
+	void getAllUsers() {
+	}
+
+	@Test
+	void find() {
+	}
+
+	@Test
+	void updateUser() {
+	}
+
+	@Test
+	void deleteUser() {
+	}
+}
